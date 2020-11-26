d2e1929829903494180d11368d8385c2c153c178
AlexGameTester <l.kirka@mail.ru>
2020-11-26 21:49:56 +0300

    Added InputWindow class

---
+++ inputwindow.py
@@ -0,0 +1,32 @@
+from manager import Manager
+
+
+class StartParameters:
+    """
+    Represents a list of parameters set by user
+    """
+    pass
+
+
+class InputWindow:
+    """
+    Represents a window that is displayed when the program is launched. User sets parameters in this window
+    """
+    def __init__(self, manager: Manager):
+        self.manager = manager
+
+    def _get_start_parameters(self) -> StartParameters:
+        """
+        Reads data from the window and transforms it into a list of parameters
+        """
+        pass
+
+    def _close(self):
+        pass
+
+    def start_calculation(self):
+        """
+        Called when user presses 'Start' button. Starts calculation of parameters
+        """
+        self.manager.start_calculation(self._get_start_parameters())
+        self._close()
--- manager.py
+++ manager.py
@@ -1,7 +1,17 @@
+from inputwindow import StartParameters
+
+
 class Manager:
 
     def start(self):
         """
         Starts execution of the program
+        """
+        pass
+
+    def start_calculation(self, params: StartParameters):
+        """
+        Starts calculation of solutions
+        :param params: a set of parameters set by user
         """
         pass
