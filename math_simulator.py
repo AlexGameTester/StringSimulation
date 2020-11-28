1d36b1236101f67394ce36fb25c0514501cf08dc
AlexGameTester <l.kirka@mail.ru>
2020-11-28 13:48:38 +0300

    Added basic values for future testing

--- math_simulator.py
+++ math_simulator.py
@@ -3,12 +3,30 @@ from typing import Callable
 from simulation import Simulation
 from simulator import Simulator
 
+import numpy as np
+
 
 class MathematicalSimulator(Simulator):
     """
     Represents a mathematical way to simulate the process
     """
+
     def __init__(self, params):
+        # TODO: Specific way to get this values will be written later
+        # assert params
+
+        self._string_length = 10
+        assert self._string_length > 0
+        self._initial_positions_x = np.linspace(0, self._string_length, 2000)
+        self._initial_positions_y = np.sin(3 * self._initial_positions_x)
+        assert len(self._initial_positions_x.shape) == 1
+        assert len(self._initial_positions_y.shape) == 1
+        assert self._initial_positions_x.shape == self._initial_positions_y.shape
+        self._initial_velocities_y = np.zeros(2000)
+        # self._number_of_points = 5000
+        # TODO: probably add data interpolation to given number of points
+
+    def _method(self):
         pass
 
     def get_method(self) -> Callable[[float], None]:
---
+++ math_simulator_tests.py
@@ -0,0 +1,5 @@
+from math_simulator import MathematicalSimulator
+
+if __name__ == "__main__":
+    math_sim = MathematicalSimulator(None)
+
