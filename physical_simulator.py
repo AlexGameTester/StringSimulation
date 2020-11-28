391ee7503f956dbc7265b1400b06048bdf4fe77e
Alexandr <alexandrmazanik@gmail.com>
2020-11-29 02:22:50 +0300

    added class for point and implemented rendering

--- physical_simulator.py
+++ physical_simulator.py
@@ -1,17 +1,30 @@
 from typing import Callable
 import math
 
+import pygame
+
 from simulation import Simulation
 from simulator import Simulator
 
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 900
+FPS = 200
+
+POINT_RADIUS = 5
+CALC_NUMBER = 100
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen.fill(WHITE)
 
 
 class Point:
-
-    def __init__(self, x, y_velocity):
-        self.x = x
+    def __init__(self, x, y, y_velocity):
+        self.x = int(x) + SCREEN_WIDTH // 3
+        self.y = int(y) + SCREEN_HEIGHT // 2
         self.velocity = y_velocity
 
 
@@ -22,12 +35,29 @@ class PhysicalSimulator(Simulator):
 
     def __init__(self, params):
         super().__init__(params)
+        self.points = []
+        with open("physical_points.txt") as points_file:
+            for data_string in points_file:
+                x, y, y_velocity = data_string.split()
+                point = Point(x, y, y_velocity)
+                self.points.append(point)
+        print(self.points)
 
     def get_method(self) -> Callable[[float], None]:
         pass
 
     def get_simulation(self) -> Simulation:
         pass
+
+    def simulate(self):
+        pass
+
+    def draw(self):
+        for point in self.points:
+            pygame.draw.circle(screen,
+                               BLACK,
+                               (point.x, point.y),
+                               POINT_RADIUS)
 
 
 def main():
@@ -37,14 +67,35 @@ def main():
 
     create_init_params(amount_of_points, length, max_velocity)
 
+    phys_sim = PhysicalSimulator(10)
+
+    clock = pygame.time.Clock()
+    finished = False
+
+    while not finished:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            elif event.type == pygame.KEYDOWN:
+                key = event.key
+                if key == pygame.K_q or key == pygame.K_ESCAPE:
+                    finished = True
+
+        phys_sim.simulate()
+        phys_sim.draw()
+
+        pygame.display.update()
+
 
 def create_init_params(amount_of_points, length, max_velocity):
     delta_r = length // amount_of_points
     x = 0
+    y = 0
     with open("physical_points.txt", "w") as points:
         for i in range(amount_of_points):
             velocity = max_velocity * math.sin(2 * math.pi * i / amount_of_points)
-            point = str(x) + " " + str(velocity) + "\n"
+            point = str(x) + " " + str(y) + " " + str(velocity) + "\n"
             points.write(point)
 
             x += delta_r
