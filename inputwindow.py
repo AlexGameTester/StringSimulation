8ff2d08fedf27de916c25a0ca9c88a12d0af2a35
CunningNobleman <45738611+CunningNobleman@users.noreply.github.com>
2020-11-30 17:29:32 +0600

    Update inputwindow.py
    1)окно - не меняет масштаб
    2)grid
    3)создает объект класса Start_Parameters
    4)как сделать закрыть окно если некоторое условие выполнено?

--- inputwindow.py
+++ inputwindow.py
@@ -1,125 +1,103 @@
-import tkinter
+from tkinter import *
+
+calculating = None
+
 class StartParameters:
-    """
     
-    Represents a list of parameters set by user
-    
-    Parameters:
-    
-    **simulation_time** - simulation runtime interval, set by user
-    **Tension** - tension exerted in the string
-    **density** - linear density of the string
-    
-    
-    """
-    pass
+    def __init__(self, density, simulation_time, tension):
+        self.density = density
+        self.simulation_time = simulation_time
+        self.tension = tension
+        self.data = [density, simulation_time, tension]
 
-
+    
 class InputWindow:
     """
     Represents a window that is displayed when the program is launched. User sets parameters in this window
     """
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    def __init__(self, root, manager):
-        self.manager = manager
-        self.root = root
-        self.root.geometry("600x400")
-        
-        canvas1 = tkinter.Canvas(self.root, width = 400, height = 300)
-        canvas1.pack()
-
-        simulation_time = tkinter.StringVar(self.root)
-        density = tkinter.StringVar(self.root)
-        tension = tkinter.StringVar(self.root)
-        
-        bottomframe = tkinter.Frame(self.root)
-        bottomframe.pack( side = tkinter.BOTTOM )
-
-
-        label1 = tkinter.Label(text = "Simultaion time")
-        self.simulation_time_entry = tkinter.Entry(self.root, textvariable=simulation_time)
-        canvas1.create_window(200, 140, window=self.simulation_time_entry)
-        canvas1.create_window(200, 100, window=label1)
-
-        label2 = tkinter.Label(text = "density")
-        self.density_entry = tkinter.Entry(self.root, textvariable=density)
-        canvas1.create_window(200, 200, window=self.density_entry)
-        canvas1.create_window(200, 160, window=label2)
-
-        label3 = tkinter.Label(text = "tension")
-        self.tension_entry = tkinter.Entry(self.root, textvariable=tension)
-        canvas1.create_window(200, 260, window=self.tension_entry)
-        canvas1.create_window(200, 220, window=label3)
-
-
-        self.start_button = tkinter.Button(bottomframe, text="Start", command=self._get_start_parameters(),   width=17, 
-                                        height=3, font=18)
-        self.start_button.pack( side = tkinter.BOTTOM)
-        
-    
-    
-
-    def _get_start_parameters(self) -> StartParameters:
+    def get_parameters(self) -> StartParameters:
         """
         Reads data from the window and transforms it into a list of parameters
         """
-        
-        
-        
+        global calculating
+
+        if len(self.density_entry.get()) == 0:
+            density_double = 1
+        elif getdouble(self.density_entry.get()) < 0:
+            density_double = 2
+        else:
+            density_double = getdouble(self.density_entry.get())
         
         if len(self.simulation_time_entry.get()) == 0:
-            simulation_time_double = 10
-        elif tkinter.getdouble(self.simulation_time_entry.get()) < 0:
-            simulation_time_double = 11
-        else :
-            a = tkinter.getdouble(self.simulation_time_entry.get())
-            simulation_time_double = a
+            simulation_time_double = 1
+        elif getdouble(self.simulation_time_entry.get()) < 0:
+            simulation_time_double = 2
+        else:
+            simulation_time_double = getdouble(self.simulation_time_entry.get())
         
-        if len(self.density_entry.get()) == 0:
-            density_double = 16
-        elif tkinter.getdouble(self.density_entry.get()) < 0:
-            density_double = 18
-        else :
-            b = tkinter.getdouble(self.density_entry.get())
-            density_double = b
-
         if len(self.tension_entry.get()) == 0:
-            tension_double = 10
-        elif tkinter.getdouble(self.tension_entry.get()) < 0:
-            simulation_time_double = 11
-        else :
-            c = tkinter.getdouble(self.tension_entry.get())
-            tension_double = c
+            tension_double = 1
+        elif getdouble(self.tension_entry.get()) < 0:
+            tension_double = 2
+        else:
+            tension_double = getdouble(self.tension_entry.get())
+        
+        c = StartParameters(density_double, simulation_time_double, tension_double)
+        
+        calculating = True
+
+        Action2(c.data) 
+        return c.data
         
         
+    def __init__(self, master, manager):
+        self.manager = manager
+        self.master = master
+        self.master.geometry('400x200')
+        self.master.resizable(width=False, height=False)
+        
+        center_frame = Frame(self.master)
+        center_frame.pack(side=TOP)
+        
+        self.label1 = Label(center_frame, text="Density")
+        self.label1.grid(row=0, column=0, pady=5, padx=5)
+        self.density_entry = Entry(center_frame)
+        self.density_entry.grid(row=0, column=1, pady=5, padx=5)
 
-        parameters = [simulation_time_double, tension_double, density_double]  
-        print(parameters)
-        return parameters
-       
+        self.label2 = Label(center_frame, text="Simulation Time")
+        self.label2.grid(row=1, column=0, pady=5, padx=5)
+        self.simulation_time_entry = Entry(center_frame)
+        self.simulation_time_entry.grid(row=1, column=1, pady=5, padx=5)
 
+        self.label3 = Label(center_frame, text="Tension")
+        self.label3.grid(row=2, column=0, pady=5, padx=5)
+        self.tension_entry = Entry(center_frame)
+        self.tension_entry.grid(row=2, column=1, pady=5, padx=5)
+
+        Button1 = Button(center_frame, command=self.get_parameters, width=10, 
+                                        height=2, font=18, text="Start")
+        Button1.grid(row=3)
+    
+    
     def _close(self):
         pass
+        
+def Action2(x):
+    print(x)
+    print("buba")
+    print("Input parameters are processing, please wait.")
+    print(calculating)
 
-    def start_calculation(self):
-        """
-        Called when user presses 'Start' button. Starts calculation of parameters
-        """
-        self.manager.start_calculation(self._get_start_parameters())
-        self._close()
+    
 
-app = tkinter.Tk()
-a = InputWindow(app, manager=1)
-app.mainloop()
+        
+
+
+
+
+App = Tk()
+a = InputWindow(App, 1)
+a.__dict__
+print(a.__dict__)
+App.mainloop()
