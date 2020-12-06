f6501c865e7794ccd4b2f8a859919f305d5420b5
AlexGameTester <l.kirka@mail.ru>
2020-12-06 19:46:16 +0300

    Deleted ImageReader class and put it's functions straight into image_reader.py

--- image_reader.py
+++ image_reader.py
@@ -3,63 +3,60 @@ import numpy as np
 from skimage import io
 
 
-class ImageReader:
-    SUPPORTED_EXTENSIONS = ['.png']
-    WHITE_GRAYSCALE = 1
+SUPPORTED_EXTENSIONS = ['.png']
+WHITE_GRAYSCALE = 1
 
-    @staticmethod
-    def _normalize_points(points: np.ndarray):
-        """
-        Rotates and moves points on complex plane to put strings ends on y=0
+def _normalize_points(points: np.ndarray):
+    """
+    Rotates and moves points on complex plane to put strings ends on y=0
 
-        :param points: numpy array of points positions represented as complex numbers
-        :return: numpy array with points where ends are on y=0
-        """
-        points -= points[0]  # make an offset to start from (0, 0)
-        first = points[0]
-        last = points[len(points) - 1]
-        diff = last - first  # vector from first point to last that represents incline of string
-        rotation = diff.conj() / abs(diff)  # complex number that rotates (abs = 1) points to neglect incline (conj
-        # makes rotation in opposite to incline direction)
-        return points * rotation
+    :param points: numpy array of points positions represented as complex numbers
+    :return: numpy array with points where ends are on y=0
+    """
+    points -= points[0]  # make an offset to start from (0, 0)
+    first = points[0]
+    last = points[len(points) - 1]
+    diff = last - first  # vector from first point to last that represents incline of string
+    rotation = diff.conj() / abs(diff)  # complex number that rotates (abs = 1) points to neglect incline (conj
+    # makes rotation in opposite to incline direction)
+    return points * rotation
 
-    @staticmethod
-    def read_points(path: str):
-        """
-        Opens an image with string and represents it with points
+def read_points(path: str):
+    """
+    Opens an image with string and represents it with points
 
-        :param path: path of the image
-        :return: a tuple (x, y) with numpy arrays of x and y points' coordinates
-        """
-        if not path:
-            raise ValueError("String path mustn't be empty")
+    :param path: path of the image
+    :return: a tuple (x, y) with numpy arrays of x and y points' coordinates
+    """
+    if not path:
+        raise ValueError("String path mustn't be empty")
 
-        file, extension = os.path.splitext(path)
-        if extension not in ImageReader.SUPPORTED_EXTENSIONS:
-            raise ValueError("File extension is not supported")
+    file, extension = os.path.splitext(path)
+    if extension not in SUPPORTED_EXTENSIONS:
+        raise ValueError("File extension is not supported")
 
-        if not os.path.exists(path):
-            raise ValueError("This path doesn't exist")
+    if not os.path.exists(path):
+        raise ValueError("This path doesn't exist")
 
-        string = io.imread(path, as_gray=True)
+    string = io.imread(path, as_gray=True)
 
-        is_white = string < ImageReader.WHITE_GRAYSCALE
+    is_white = string < WHITE_GRAYSCALE
 
-        top_nonwhite = is_white.argmax(axis=0)
-        bot_nonwhite = len(is_white) - np.flipud(is_white).argmax(axis=0)
+    top_nonwhite = is_white.argmax(axis=0)
+    bot_nonwhite = len(is_white) - np.flipud(is_white).argmax(axis=0)
 
-        points_y = len(is_white) / 2 - 1/2 * (top_nonwhite + bot_nonwhite)  # minus is added because coordinate
-        # system is flipped
-        points_x = np.array([i for i in range(len(points_y))])
-        complex_points = points_x + 1j * points_y  # represent points as complex numbers
-        complex_points = ImageReader._normalize_points(complex_points)
+    points_y = len(is_white) / 2 - 1/2 * (top_nonwhite + bot_nonwhite)  # minus is added because coordinate
+    # system is flipped
+    points_x = np.array([i for i in range(len(points_y))])
+    complex_points = points_x + 1j * points_y  # represent points as complex numbers
+    complex_points = _normalize_points(complex_points)
 
-        return complex_points.real, complex_points.imag
+    return complex_points.real, complex_points.imag
 
 
 def main():
     path = 'test_image.png'
-    x, y = ImageReader.read_points(path)
+    x, y = read_points(path)
     import matplotlib.pyplot as plt
     plt.plot(x, y)
     plt.show()
